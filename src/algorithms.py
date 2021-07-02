d64217b568dc96c2fd6ef8cf114402db4e2824f3
ChiaraBi <chiara.bigarella1@gmail.com>
2021-07-02 15:46:07 +0200

    changes

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 
 #TODO: what is F function?
@@ -6,7 +5,8 @@ import numpy as np
 #TODO: does the worker sample the data points or is the same of the master x_t?
 #TODO: x0 is the 0? which point is the starting point? is a feasible but which one is it?
 
-def decentralized_stochastic_gradient_free_FW(data_workers, F, m, T, M,epsilon,d):
+
+def decentralized_stochastic_gradient_free_FW(data_workers, y, F, m, T, M, epsilon, d):
     """
     :param data: images
     :param y: labels
@@ -16,44 +16,46 @@ def decentralized_stochastic_gradient_free_FW(data_workers, F, m, T, M,epsilon,d
     :param M: number of workers
     :return: universal perturbation
     """
-    #starting point, x is the perturbation
-    delta = np.zeros(d) # starting point: delta_0
+    # starting point, x is the perturbation
+    delta = np.zeros(d)  # starting point: delta_0
     delta_history = []
-    g_worker = np.array(M) # should hold precedent g worker, handled by master.
+    gradient_worker = np.zeros((M, d))  # should hold workers' precedent g, handled by master.
     for t in range(0, T):
+        ro = 4 / ((1 + d / m) ** (1 / 3) * (t + 8) ** (2 / 3))
+        c = 2 * (m) ** (1 / 2) / (d ** (3 / 2) * (t + 8) ** (1 / 3))
+
         for w_idx in range(0, M):
-            g_worker[w_idx] = decentralized_worker_job(d, t, m, g_worker[w_idx], F, delta,y)
+            gradient_worker[w_idx] = decentralized_worker_job(data_workers[w_idx, :, :, :, :], y, d, ro, c, m,
+                                                              gradient_worker[w_idx, :], F, delta)
 
         #wait all workers computation
         g = np.average(g_worker)
         vt = - epsilon * np.sign(g)
         gamma = 2 / (t + 8)
-        delta =  (1-gamma) * delta + gamma * vt
+        delta = (1-gamma) * delta + gamma * vt
         delta_history.append(delta)
         # send to all nodes
         # what the fuck is x for worker and for master???? Worker should sample from the data points!!!??
-    return delta, delta_history
+    return delta_history
 
 
-def decentralized_worker_job(d, t, m, g, F, delta, y):
-    '''
+def decentralized_worker_job(data, y, d, ro, c, m, g_prec, F, delta):
+    """
     :param d: dimension
-    :param t:
+    :param ro:
+    :param c:
     :param m: directions
     :param g: master g,  precedent
     :param F: loss function to minimize
     :return:
-    '''
-    z = np.random.normal(loc=0.0, scale=1.0, size=(d, d))
+    """
+    z = np.random.normal(loc=0.0, scale=1.0, size=(d, 1))
 
-    ro = 4/((1+d/m)**(1/3) * (t+8)**(2/3))
-    c = 2 * (m)**(1/2) / (d**(3/2) * (t+8)**(1/3))
+    g = 1/m * (np.sum((F(data + delta + c * z, y) - F(data + delta, y)) / c) * z)  # element wise product inside the summation, then i apply the sum
 
-    g_prec = g.copy()
-    data_point = sample_data_point()
+    if g_prec != np.zeros((1, d)):
+        g = (1 - ro) * g_prec + ro * g
 
-    g = 1/m * (np.sum((F(x + c * z, y) - F(data_point + delta, y))/ c) * z) # element wise product inside the summation, then i apply the sum
-    g = (1 - ro) *  g_prec + ro * g
     return g
 
 def sample_data_point():
--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
+from algorithms import *
 from lenet5 import *
 import numpy as np
-import algorithms
 
 _, (test_x, test_y) = load_MNIST()
 
@@ -21,6 +21,7 @@ labels = test_y[indexes]
 
 print(len(labels))  # 9826
 
+# M = num workers; we consider a number of workers <=10
 M = 10
 data_per_classes = []
 for label_class in range(0, 10):
@@ -29,17 +30,26 @@ for label_class in range(0, 10):
 data_per_classes = np.array(data_per_classes)
 data_workers = []
 
-
-
-#TODO: controllare che torni a me sembra di si, sarebbe piu bello generalizzare meglio facendo la divisone intera.
-# Ad ogni 10 colonne formo un worker delle immagini di diverse label formo un worker
-idx = 0
-for j in range(0, 10):
+for offset in range(0, 100, M):
     image_worker = []
-    for i in range(0, 10):
-        image_worker.extend(data_per_classes[:, idx, :, :, :])
-        idx = idx + 1
+    for c in range(0, 10):
+        image_worker.extend(data_per_classes[c, offset:offset+M, :, :, :])
     data_workers.append(image_worker)
 
-print(np.array(data_workers).shape) # all 10 workers, now have 100 images each of 10 classes.
-# I think it does work now.
+print(np.array(data_workers).shape)  # now all 10 workers have 100 images, 10 for each class.
+
+y = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+y = np.repeat(y, 10)
+print(y)
+
+gradient_worker = np.zeros((M, d))
+print(gradient_worker.shape)
+# print(gradient_worker[0, :] != np.zeros((1, d)))
+
+if np.array_equal(gradient_worker[0, :], np.zeros((1, d))):
+    print("ok")
+
+
+if gradient_worker[0, :] is np.zeros((1, d)):
+    print("ok 2")
+
