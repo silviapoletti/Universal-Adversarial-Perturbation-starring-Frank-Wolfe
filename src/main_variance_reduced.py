4e8ee1f738b8792a1faa920cfaf7ae5a52b1d1f0
GurjeetSinghSangra <sardargurjeet95@gmail.com>
2021-07-05 11:05:48 +0200

    Changed tmp file to main variance reduced main, and added codes for testing

--- src/main_variance_reduced.py
+++ src/main_variance_reduced.py
@@ -3,6 +3,24 @@ from lenet5 import *
 import numpy as np
 import matplotlib.pyplot as plt
 
+def plot_perturbation(perturbation, m):
+  fig, ax = plt.subplots(figsize=(12, 12))
+  img = plt.imshow(perturbation.reshape((28, 28)))
+  fig.colorbar(img, ax=ax, fraction=0.03, pad=0.05)
+  plt.savefig(f"./perturbation_variance_{m}", bbox_inches="tight")
+  plt.show()
+
+def plot_perturbated_img(perturbation, m):
+  image = test_x[5].numpy().reshape(28,28)
+  img_noise = image + perturbation.reshape((28, 28))
+  fig, ax = plt.subplots(1, 2,figsize=(5, 5))
+  a = ax[0].imshow(image, cmap='Greys')
+  b = ax[1].imshow(img_noise, cmap='Greys')
+  fig.colorbar(a, ax=ax[0], fraction=0.03, pad=0.05)
+  fig.colorbar(b, ax=ax[1], fraction=0.03, pad=0.05)
+  plt.savefig(f"./image_perturbation_variance_example_{m}", bbox_inches="tight")
+  plt.show()
+
 _, (test_x, test_y) = load_MNIST()
 
 path = './data/lenet5'
@@ -45,13 +63,24 @@ images = data[:1000]
 targets = labels[:1000]
 n = 5
 S2 = 3
-q = 5
+q = 7
 S1 = len(images)
-M = 10
-T = 10
+M = 5
+T = 20
 epsilon = 0.25
 d = 784
 
 
 delta = decentralized_variance_reduced_zo_FW(images, targets, lenet5.negative_loss, S2, T, M, n, epsilon, d, q, S1)
-np.save(f"./data/perturbation_variance", delta[-1])
+np.save(f"./data/perturbation_variance_{M}_{T}", delta[-1])
+delta = delta[-1]
+
+perturbation = np.tile(delta, data.shape[0])
+perturbation = perturbation.reshape((data.shape[0],28,28,1))
+perturbated_data = data + perturbation
+perturbated_data = np.clip(perturbated_data, 0., 1.)
+
+loss = lenet5.model.evaluate(perturbated_data, labels)
+plot_perturbation(delta, M)
+plot_perturbated_img(delta, M)
+#loss_history.append(loss)
